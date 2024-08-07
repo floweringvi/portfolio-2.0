eecd230771cd98366a6ba103be5388c33e02e396
Violet Stanziani <stanzianiviolet@gmail.com>
2024-08-06 21:26:34 -0400

    finished main.jsx setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,32 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App';
+import Home from './pages/Home';
+import Error from './pages/Error';
+import Contact from './pages/Contact';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <Error />,
+        children: [
+            {
+                index:true,
+                element: <Home />,
+            },
+            {
+                path:'/Contact',
+                element: <Contact/>
+            },
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+    <RouterProvider router={router} />
+  ); 
