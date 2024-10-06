f72569c806ce467934313d37c6a375ef26055a63
Violet Stanziani <stanzianiviolet@gmail.com>
2024-10-06 12:11:43 -0400

    made hero a carousel, changed size of buttons on portfolio header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,56 @@
 import React from 'react';
 
 import HeroI from '../assets/images/heroimage.png'
+import SVG from '../assets/images/SVG-Test.png'
+import Ecomm from '../assets/images/e-commerce.png'
+import Fetch from '../assets/images/dadDinnerFetch.png'
+
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const Hero =() => {
     return(
-        <div className="hero">
+      <Carousel swipeable={true} showThumbs={false} autoPlay={true} transitionTime={500}>
+        <div className="hero" >
         <img
           src={HeroI}
           alt="Image of code from craft corner"
-          className="hero-image"
+          className="hero-image-start"
         />
         <div className="hero-content">
           <p id="hero-sub" className="handjet hero-font" >Full-Stack Development</p>
           <div>
-            <h1 role="button" href="/" className="handjet hero-font" id="hero-title">
+            <h1 className="handjet hero-font" id="hero-title">
               Violet Stanziani
             </h1>
           </div>
         </div>
       </div>
+      <div className="hero">
+        <img
+        src={SVG}
+        alt=""
+        className="hero-image"
+        />
+        
+
+      </div>
+      <div className="hero">
+        <img
+        src={Ecomm}
+        alt=""
+        className="hero-image"
+        />
+
+      </div>
+      <div className="hero">
+        <img
+        src={Fetch}
+        className="hero-image"
+        />
+        
+      </div>
+      </Carousel>
     )
 }
 export default Hero;
